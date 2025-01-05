51928c0543d119cb96fcecb675736302f7437963
takatama <takatama@users.noreply.github.com>
2025-01-05 13:06:37 +0900

    refactor: simplify index.tsx by removing Layout and Top components for direct rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import { Hono } from 'hono'
-import type { FC } from 'hono/jsx'
 
 const app = new Hono()
 
-const Layout: FC = (props) => {
-  return (
+app.get('/', (c) => {
+  return c.html(
     <html>
       <head>
         <link href="/static/style.css" rel="stylesheet" />
@@ -15,26 +14,6 @@ const Layout: FC = (props) => {
       </body>
     </html>
   )
-}
-
-const Top: FC<{ messages: string[] }> = (props: {
-  messages: string[]
-}) => {
-  return (
-    <Layout>
-      <h1>Hello Hono!</h1>
-      <ul>
-        {props.messages.map((message) => {
-          return <li>{message}!!</li>
-        })}
-      </ul>
-    </Layout>
-  )
-}
-
-app.get('/', (c) => {
-  const messages = ['Good Morning', 'Good Evening', 'Good Night']
-  return c.html(<Top messages={messages} />)
 })
 
 export default app
